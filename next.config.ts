491ecc0808b735e7da2aa3df24ee000d54b51685
Onzoro-rento <rento0725ken1009@icloud.com>
2026-05-03 11:17:25 +0900

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
